d89da8f2aa293ae90e6d82a9b11dc164f018eee2
Janus Maritz <janus.maritz7@gmail.com>
2025-08-01 11:25:59 +0200

    Add production build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KI4N5CU6.mjs";import"./chunk-ZQJHMSP7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-EZ3ULDPW.mjs";import"./chunk-ZQJHMSP7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
